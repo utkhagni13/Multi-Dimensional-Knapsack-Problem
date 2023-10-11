c1c95433983eec4706215a47bafeb1fc925c03ab
Utkarsh Agnihotri <utkhagni13@gmail.com>
2023-10-11 11:22:53 +0530

    added codes and data

--- code.cpp
+++ code.cpp
@@ -9,18 +9,29 @@ int *CAPACITIES;        // populated automatically by processData
 int **CONSTRAINTS;      // populated automatically by processData
 int *VALUES;            // populated automatically by processData
 
-const int POPULATION = 10;        // size of the population
-const int LOCAL_IMPROVEMENT = 10; // number of local improvements
-const int GENERATIONS = 100;      // number of generations to run the algorithm
+const int POPULATION_SIZE = 100; // size of the population
+const int GENERATIONS = 10000;   // number of generations to run the algorithm
+const double ALPHA = 0.787;
+double pm = 0.2;
 
-int *WEIGHTED_SUMS; // weighted sum for the corresponding constraint
+pair<int, int> CORE; // size of our core
+int *WEIGHTED_SUMS;  // weighted sum for the corresponding constraint
+vector<int> X1;
 
 struct Item
 {
     int value;
+    int objId;
     double efficiency;
 };
 vector<Item> ITEMS; // efficiencies of the objects
+
+struct Chromosome
+{
+    vector<int> selected_items;
+    double fitness;
+};
+vector<Chromosome> Population; // Population
 
 void processData(char *filename)
 {
@@ -188,20 +199,6 @@ void processData(char *filename)
     OPTIMUM = atoi(tok);
 
     delete[] (line);
-
-    // for (int i = 0; i < NUMBER_OBJECTS; ++i)
-    //     printf("%d: %d\n", i, VALUES[i]);
-    // cout << endl;
-    // for (int i = 0; i < NUMBER_CONSTRAINTS; ++i)
-    //     printf("%d: %d\n", i, CAPACITIES[i]);
-    // cout << endl;
-    // for (int i = 0; i < NUMBER_CONSTRAINTS; ++i)
-    // {
-    //     printf("%d:\n", i);
-    //     for (int j = 0; j < NUMBER_OBJECTS; ++j)
-    //         printf("%d: %d, ", j, CONSTRAINTS[i][j]);
-    //     cout << endl;
-    // }
 }
 
 /* Evaluating the weighted sums */
@@ -243,19 +240,192 @@ void evaluateEfficiencies()
         {
             new_weight += (ratios[j] * CONSTRAINTS[j][i]);
         }
-        ITEMS.push_back({VALUES[i], VALUES[i] / new_weight});
+        ITEMS.push_back({VALUES[i], i + 1, VALUES[i] / new_weight});
     }
 }
 
 /* Sorting the solutions by their efficiencies */
 bool compareItem(Item &i1, Item &i2)
 {
-    return (i1.efficiency < i2.efficiency);
+    return (i1.efficiency > i2.efficiency);
 }
 void sortByEfficiencies()
 {
     evaluateEfficiencies();
     sort(ITEMS.begin(), ITEMS.end(), compareItem);
+}
+
+inline void setCoreSize()
+{
+    CORE.first = ceil(NUMBER_OBJECTS / 3) - 1;
+    CORE.second = ceil((2 * NUMBER_OBJECTS) / 3) - 1;
+}
+
+void setX1()
+{
+    X1.resize(NUMBER_OBJECTS);
+    for (int i = 0; i <= CORE.first; i++)
+    {
+        X1[ITEMS[i].objId - 1] = 1;
+    }
+}
+
+double calc_fitness(vector<int> &chromosome)
+{
+    double fitness = 0;
+    double jaccardSimilarity = 0;
+    int intersection = 0;
+    int Union = 0;
+    int valueSum = 0;
+    for (int i = 0; i < NUMBER_OBJECTS; i++)
+    {
+        if (X1[i] and chromosome[i])
+        {
+            intersection++;
+        }
+        if (X1[i] or chromosome[i])
+        {
+            Union++;
+        }
+    }
+    jaccardSimilarity = (1.0 * intersection) / Union;
+    for (int i = 0; i < NUMBER_OBJECTS; i++)
+    {
+        if (chromosome[i] == 1)
+        {
+            valueSum += VALUES[i];
+        }
+    }
+    jaccardSimilarity *= 100;
+    fitness = (1 + jaccardSimilarity) * valueSum;
+    return fitness;
+}
+
+void initializePopulation()
+{
+    for (int i = 0; i < POPULATION_SIZE; i++)
+    {
+        vector<int> chromosome(NUMBER_OBJECTS, 0);
+        unordered_set<int> set;
+        for (int j = 0; j <= CORE.first; j++)
+        {
+            double alpha_temp = (float)rand() / RAND_MAX;
+            if (alpha_temp <= ALPHA)
+            {
+                chromosome[ITEMS[j].objId - 1] = 1;
+            }
+        }
+        int remaining_genes = ceil(NUMBER_OBJECTS * 0.08);
+        int count = 0;
+        while (count < remaining_genes)
+        {
+            int select = (CORE.first + 1) + (rand() % ((2 * NUMBER_OBJECTS) / 3));
+            if (set.find(select) == set.end())
+            {
+                chromosome[ITEMS[select].objId - 1] = 1;
+                set.insert(select);
+                count++;
+            }
+        }
+        double fitness = calc_fitness(chromosome);
+        Chromosome C;
+        C.selected_items = chromosome;
+        C.fitness = fitness;
+        Population.push_back(C);
+    }
+}
+
+// Define the selection function
+int random(int low, int high)
+{
+    return low + rand() % (high - low + 1);
+}
+void parentsSelection(vector<int> &mating_pool)
+{
+    // Use tournament selection
+    int tournament_size = 5, curr = 0;
+    while (curr < POPULATION_SIZE)
+    {
+        int best = 0;
+        double best_fit = -1;
+
+        // generate distinct K(tournament_size) random numbers from 1 to Population size
+        vector<int> rand_nums;
+        for (int i = 0; i < tournament_size; i++)
+        {
+            rand_nums.push_back(random(0, POPULATION_SIZE - 1));
+        }
+        // select the best solution from these random numbers indexes from the Population
+        for (int i = 0; i < tournament_size; i++)
+        {
+            if (best_fit < Population[rand_nums[i]].fitness)
+            {
+                best_fit = Population[rand_nums[i]].fitness;
+                best = rand_nums[i];
+            }
+        }
+        mating_pool[curr] = best;
+        curr++;
+    }
+}
+
+// apply crossover on the mating pool
+void crossover(vector<Chromosome> &offsprings, vector<int> &mating_pool)
+{
+    int curr = 0;
+    while (curr < POPULATION_SIZE / 2)
+    {
+        int parent1_index = mating_pool[rand() % POPULATION_SIZE];
+        int parent2_index = mating_pool[rand() % POPULATION_SIZE];
+
+        Chromosome child1, child2;
+        // find a crossover point from 1 to Chromosome.size()-1
+        int crossover_point = (rand() % (NUMBER_OBJECTS - 1)) + 1;
+        child1.selected_items.resize(NUMBER_OBJECTS);
+        child2.selected_items.resize(NUMBER_OBJECTS);
+
+        for (int i = 0; i < crossover_point; i++)
+        {
+            child1.selected_items[i] = Population[parent1_index].selected_items[i];
+            child2.selected_items[i] = Population[parent2_index].selected_items[i];
+        }
+        for (int i = crossover_point; i < NUMBER_OBJECTS; i++)
+        {
+            child1.selected_items[i] = Population[parent2_index].selected_items[i];
+            child2.selected_items[i] = Population[parent1_index].selected_items[i];
+        }
+
+        offsprings.push_back(child1);
+        offsprings.push_back(child2);
+        curr++;
+    }
+}
+
+void mutation(vector<Chromosome> &offspring)
+{
+    for (int i = 0; i < offspring.size(); i++)
+    {
+        for (int j = 0; j < NUMBER_OBJECTS; j++)
+        {
+            double p = (float)rand() / RAND_MAX;
+            if (p <= pm)
+            {
+                if (offspring[i].selected_items[j] == 1)
+                {
+                    offspring[i].selected_items[j] = 0;
+                }
+                else
+                {
+                    offspring[i].selected_items[j] = 1;
+                }
+            }
+        }
+    }
+}
+
+bool cmp(Chromosome &c1, Chromosome &c2)
+{
+    return (c1.fitness > c2.fitness);
 }
 
 int main(int argc, char **argv)
@@ -269,8 +439,6 @@ int main(int argc, char **argv)
     }
     if (strcmp(argv[2], "weing") == 0)
     {
-        printf("Data file format: weing\n");
-        printf("Data file name: %s\n", argv[1]);
         processData(argv[1]);
     }
     else
@@ -279,11 +447,166 @@ int main(int argc, char **argv)
         exit(1);
     }
 
+    /* Initialize the random number generator */
+    srand((unsigned)time(NULL));
+
     // efficiency calculation of the values
     sortByEfficiencies();
 
+    // divide items into X1, Core and X0
+    setCoreSize();
+    setX1();
+
+    /********* APPLYING GENETIC ALGORITHM *********/
+
+    // create population
+    initializePopulation();
+
+    int n = 0;
+    Chromosome global_best;
+    global_best.fitness = -1;
+    while (n < GENERATIONS)
+    {
+        // Elitism
+
+        sort(Population.begin(), Population.end(), cmp);
+        int lambda = ceil((NUMBER_OBJECTS * 10) / 100);
+
+        // int elitism_threshold = 620000;
+        vector<int> elites;
+        for (int i = 0; i < lambda; i++)
+        {
+            elites.push_back(i);
+        }
+
+        // parental selection for creating mating pool
+        vector<int> mating_pool(POPULATION_SIZE);
+        parentsSelection(mating_pool);
+
+        // apply crossover on the mating pool elements
+        vector<Chromosome> offsprings;
+        crossover(offsprings, mating_pool);
+
+        // apply mutation on the offsprings
+        mutation(offsprings);
+
+        // evaluate fitness for the offsprings
+        for (int i = 0; i < POPULATION_SIZE; i++)
+        {
+            offsprings[i].fitness = calc_fitness(offsprings[i].selected_items);
+        }
+
+        // find the best solution of the current generation
+        double best = 0, idx = 0;
+        for (int i = 0; i < POPULATION_SIZE; i++)
+        {
+            if (best < offsprings[i].fitness)
+            {
+                best = offsprings[i].fitness;
+                idx = i;
+            }
+        }
+        if (global_best.fitness < best)
+        {
+            global_best.selected_items.resize(NUMBER_OBJECTS);
+            global_best.fitness = best;
+            for (int i = 0; i < NUMBER_OBJECTS; i++)
+            {
+                global_best.selected_items[i] = offsprings[idx].selected_items[i];
+            }
+        }
+
+        // generational model
+        sort(offsprings.begin(), offsprings.end(), cmp);
+        for (int i = 0; i < elites.size(); i++)
+        {
+            Population[i] = Population[elites[i]];
+        }
+        for (int i = elites.size(); i < POPULATION_SIZE; i++)
+        {
+            Population[i] = offsprings[i - elites.size()];
+        }
+        n++;
+    }
+    cout << "The global best solution's fitness: " << global_best.fitness << endl;
+    cout << "The global best solution is: " << endl;
+    long long max_value = 0;
     for (int i = 0; i < NUMBER_OBJECTS; i++)
     {
-        printf("%d: %d\t%f\n", i, ITEMS[i].value, ITEMS[i].efficiency);
+        if (global_best.selected_items[i])
+        {
+            cout << i + 1 << " ";
+            max_value += (VALUES[i]);
+        }
     }
+    cout << endl
+         << "The max value of the knapsack is: " << max_value << endl
+         << OPTIMUM << endl;
+
+    return 0;
 }
+
+/* Printing the contraints, values, capacities */
+
+// for (int i = 0; i < NUMBER_OBJECTS; ++i)
+//     printf("%d: %d\n", i, VALUES[i]);
+// cout << endl;
+// for (int i = 0; i < NUMBER_CONSTRAINTS; ++i)
+//     printf("%d: %d\n", i, CAPACITIES[i]);
+// cout << endl;
+// for (int i = 0; i < NUMBER_CONSTRAINTS; ++i)
+// {
+//     printf("%d:\n", i);
+//     for (int j = 0; j < NUMBER_OBJECTS; ++j)
+//         printf("%d: %d, ", j, CONSTRAINTS[i][j]);
+//     cout << endl;
+// }
+
+/* Printing Chromosome */
+
+// for(int j = 0; j < chromosome.size(); j++) {
+//     printf("%d: %d\n", j, chromosome[j]);
+// }
+
+/* Printing Items */
+
+// for (int i = 0; i < NUMBER_OBJECTS; i++)
+// {
+//     printf("%d: %d\t%d\t%f\n", i, ITEMS[i].value, ITEMS[i].objId, ITEMS[i].efficiency);
+// }
+
+/* Elitism by threshold value */
+
+// for (int i = 0; i < POPULATION_SIZE; i++)
+// {
+//     if (Population[i].fitness > elitism_threshold)
+//     {
+//         elites.push_back(i);
+//     }
+// }
+
+/* Printing Population */
+
+// for (int i = 0; i < POPULATION_SIZE; i++)
+// {
+//     printf("%d: %f\n", i + 1, offsprings[i].fitness);
+//     for (int j = 0; j < NUMBER_OBJECTS; j++)
+//     {
+//         printf("%d ", offsprings[i].selected_items[j]);
+//     }
+//     cout << endl
+//          << endl;
+// }
+
+/* Printing Generations */
+
+// printf("Generation number %d: %f\n", n, best);
+// for (int i = 0; i < NUMBER_OBJECTS; i++)
+// {
+//     if (offsprings[idx].selected_items[i])
+//     {
+//         printf("%d, ", i + 1);
+//     }
+// }
+// cout << endl;
+// cout << endl;
